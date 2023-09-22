94d46d4c3ce2898e996e2a08c584123154fa5385
eeeXun <sdes96303@gmail.com>
2023-09-22 12:14:08 +0800

    patch statusallmons

--- config.h
+++ config.h
@@ -15,6 +15,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int statusallmons      = 1;        /* 0 means no statusbar on all monitors */
 static const int focusonwheel       = 0;
 static const char *fonts[]          = {
     "Monospace:size=15", "Hack Nerd Font:size=15",
